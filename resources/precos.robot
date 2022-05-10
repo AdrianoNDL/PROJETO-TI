7e5646b3c272b6f76e5010c756c8690df17974e1
AdrianoNDL <adrianondl0@gmail.com>
2022-05-09 23:25:14 -0300

    inserir registro no banco de dados

--- resources/precos.robot
+++ resources/precos.robot
@@ -4,3 +4,4 @@ Resource    ../keywords/planilha.robot
 Resource    ../keywords/kabum.robot
 Resource    ../keywords/pichau.robot
 Resource    ../keywords/nova_era.robot
+Resource    ../keywords/mongo.robot
--- variables/Global.py
+++ variables/Global.py
@@ -1,1 +1,11 @@
+#Decouple = Biblioteca para ler o arquivo .env, o aqruivo que contem senha
 from decouple import config
+#Datetime = Pacote padrão do Python para trabalhar com Data/Tempo
+from datetime import datetime
+
+agora = datetime.now().strftime("%Y-%m-%d %H:%M:00")
+host = config("host")
+port = config("port", cast=int)
+user = config("user", default=None)
+password = config("password", default=None)
+database = config("database")
